b7ddbb7c5a7195507549bcea1510665df1f195df
Cody Chase <cchase1991@gmail.com>
2024-06-19 19:07:40 -0400

    changed baseurl

--- client/src/hooks/API.js
+++ client/src/hooks/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001';
-// const baseUrl = 'https://tame-the-list.onrender.com';
+// const baseUrl = 'http://localhost:3001';
+const baseUrl = 'https://tame-the-list.onrender.com';
 
 const instance = axios.create({
     baseURL: baseUrl,
