e676f42b81a80960f7e9f676f7f17d46e9f16b01
Cody Chase <cchase1991@gmail.com>
2024-06-19 19:15:36 -0400

    Revert "server/package changes"

--- client/src/pages/boards.jsx
+++ client/src/pages/boards.jsx
@@ -4,8 +4,6 @@ import '../index.css';
 import { useCookies } from 'react-cookie';
 import instance from '../hooks/API.js';
 
-import AddBoard from '../components/modals/addBoard.jsx';
-
 export const Boards = () => {
 	const [boards, setBoards] = useState([]);
 	const navigate = useNavigate();
@@ -30,7 +28,6 @@ export const Boards = () => {
 			<h1 className='text-xl font-bold text-blue-light text-center my-5'>
 				Boards
 			</h1>
-			<AddBoard />
 			<div className='grid grid-cols-4 gap-4'>
 				{boards.map((board) => (
 					<div
